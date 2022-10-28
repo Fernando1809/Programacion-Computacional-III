fffdba40d0768f0a604cfc8c3832cf8876feb70f
Fernando1809 <ferandre1718.frg@gmail.com>
2022-10-28 11:18:19 -0600

    Terminacion del proyecto de Python como backend y IA

--- server.py
+++ server.py
@@ -1,7 +1,34 @@
 from urllib import parse
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sb
+
 port = 3000
+
+temperaturas = pd.read_csv('temperaturas.csv', sep=";")
+print(temperaturas)
+
+#datos de entrenamiento
+k = temperaturas["k"]
+c = temperaturas["c"]
+
+#modelo de entrenamiento, con 1 capa densa (Oculta)
+modelo = tf.keras.Sequential()
+modelo.add(tf.keras.layers.Dense(units=1, input_shape=[1]))
+
+#compilar el modelo
+modelo.compile(optimizer=tf.keras.optimizers.Adam(1), loss="mean_squared_error")
+
+#entrenar el modelo
+epocas = modelo.fit(k,c, epochs=200, verbose=0)
+
+#probar nuestra IA
+resp = modelo.predict([5778])
+print(resp)
+
 
 class miServidor(SimpleHTTPRequestHandler):
     def do_GET(self):
