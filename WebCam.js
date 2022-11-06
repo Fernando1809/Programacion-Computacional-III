2ed8c46f961070add7941f178da95b15eed4178b
Fernando1809 <ferandre1718.frg@gmail.com>
2022-11-06 16:52:26 -0600

    Implementacion de la camara

---
+++ WebCam.js
@@ -0,0 +1,40 @@
+'use strict'; // Para hacer que el código sea mas seguro.
+
+// Definimos las constantes que vamos a utilizar
+const videoFrame = document.getElementById('video_frame');
+const canvasFrame = document.getElementById('canvas_frame');
+const snapFrame = document.getElementById("snap_frame");
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+// Definimos tamaño del video y si queremos audio o no
+const constraints = {
+    audio: true,
+    video: {
+        width: 720, height: 405
+    }
+};
+
+// Comprobamos acceso a la Webcam
+async function init() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        handleSuccess(stream);
+    } catch (e) {
+        errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+    }
+}
+
+// En caso de que el acceso sea correcto, cargamos la webcam
+async function handleSuccess(stream) {
+    window.stream = stream;
+    videoFrame.srcObject = stream;
+}
+
+// Iniciamos JS
+init();
+
+// Hacemos captura de pantalla al hacer click
+var context = canvasFrame.getContext('2d');
+snapFrame.addEventListener("click", function() {
+    context.drawImage(videoFrame, 0, 0, 320, 140);
+});
